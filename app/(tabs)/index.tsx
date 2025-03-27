6d5eb04c5e414fd2d828c9d2be59833451857b66
Valeska <vah.tamachunas@gmail.com>
2025-03-27 12:29:52 -0300

    Criando componente Categories

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,36 @@
 import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Category } from "@/components/index";
-import { categories } from "@/utils/categories";
+import { Categories } from "@/components/categories";
 
 export default function Index() {
   return (
-  <View style={styles.container}>
- <View style={styles.header}>
-<Image style={styles.logo} source={require('@/assets/logo.png')}  />
-<TouchableOpacity>
-<MaterialIcons name="add" size={32} color="#a89907"/>
-</TouchableOpacity>
- </View>
-
- <Category name={categories[0].name} icon={categories[0].icon} isSelected/>
- <Category name={categories[1].name} icon={categories[1].icon} isSelected/>
- <Category name={categories[2].name} icon={categories[2].icon} isSelected/>
-
-  </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={require("@/assets/logo.png")} />
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color="#a89907" />
+        </TouchableOpacity>
+      </View>
+      <Categories />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header:{
+  header: {
     paddingHorizontal: 24,
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
-    alignItems: 'center', 
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 32,
-
   },
-  container:{
+  container: {
     flex: 1,
     paddingTop: 52,
   },
   logo: {
     width: 32,
-    height: 38
+    height: 38,
   },
-  
 });
---
+++ app/components/categories/index.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, FlatList } from "react-native";
+import { Category } from "@/components/category";
+import { categories } from "@/utils/categories";
+
+export function Categories() {
+  return (
+    <FlatList
+      data={categories}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <Category name={item.name} icon={item.icon} isSelected={false} />
+      )}
+      horizontal
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 52,
+    maxHeight: 52,
+  },
+  content: {
+    gap: 16,
+    paddingHorizontal: 24,
+  },
+});
